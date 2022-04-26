36d6692af07db6d539feae59844a2bb8e444635b
Rodolfo Guerra <rodolfoguerraster@gmail.com>
2022-04-26 14:36:32 -0300

    Criado user service

--- src/test/java/com/wallet/WalletApplicationTests.java
+++ src/test/java/com/wallet/WalletApplicationTests.java
@@ -14,5 +14,4 @@ class WalletApplicationTests {
     @Test
     void contextLoads() {
     }
-
 }
---
+++ src/test/java/com/wallet/service/UserServiceTest.java
@@ -0,0 +1,43 @@
+package com.wallet.service;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.BDDMockito;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import com.wallet.entity.User;
+import com.wallet.repository.UserRepository;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@ActiveProfiles(value = "test")
+class UserServiceTest {
+
+    @MockBean
+    UserRepository repository;
+
+    @Autowired
+    UserService service;
+
+    @BeforeEach
+    public void setUp() {
+	BDDMockito.given(repository.findByEmailEquals(Mockito.anyString())).willReturn(Optional.of(new User()));
+    }
+
+    @Test
+    void testFindByEmail() {
+	Optional<User> user = service.findByEmail("email@teste.com");
+
+	assertTrue(user.isPresent());
+    }
+}
